aa0ce813dbaebf685ee08e1a583f7d2fdef259cb
kyle <kyle@localhost>
2020-02-27 16:56:14 -0700

    init commit

---
+++ ScriptEngine Testing/Useful_Methods/EvalScript.java
@@ -0,0 +1,18 @@
+
+
+import java.util.ArrayList;
+
+import useful_methods.MyMethods;
+
+public class EvalScript {
+    public static void main(String[] args) throws Exception {
+       MyMethods.speak("");
+
+    	
+    }
+
+
+
+
+    
+}
